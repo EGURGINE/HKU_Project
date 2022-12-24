8f62a1a3050e8c8f58a06acfaf4e24567115d8f6
Junho <sdh210202@sdh.hs.kr>
2022-12-24 23:33:15 +0900

    Jump 개발중

--- Assets/Junho/Scripts/Player.cs
+++ Assets/Junho/Scripts/Player.cs
@@ -6,14 +6,38 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float spd;
     [SerializeField] private Rigidbody rb;
+
+
+    private RaycastHit hit;
+    [SerializeField] private float jumpDistance;
+    [SerializeField] private float jumpPower;
+    private bool isJumped = false;
     void Update()
     {
-        Move(); 
+        Move();
+
+        if (isJumped == true) return;
+        JumpCheck();
     }
 
+    private void JumpCheck()
+    {
+        Debug.DrawRay(transform.position, Vector2.down * jumpDistance, Color.blue);
+        if (Physics.Raycast(transform.position, Vector2.down, out hit, jumpDistance))
+        {
+            if (hit.collider.CompareTag("Ground") && isJumped == false)
+                isJumped = false;
+        }
+    }
     private void Move()
     {
         float x = Input.GetAxis("Horizontal") * spd * Time.deltaTime;
         transform.Translate(new Vector3(x,0,0));
-    }
+        if (Input.GetKeyDown(KeyCode.Space) && isJumped == false)
+        {
+            isJumped = true;
+            rb.AddForce(new Vector2(x * 0.9f , jumpPower), ForceMode.Impulse);
+        }
+    }   
+
 }
