910c396b9f2fa16c31829e216c5729023addbb0f
dleodns1591 <88140817+dleodns1591@users.noreply.github.com>
2023-01-03 12:57:52 +0900

    1~6 장애물 완성
    1~6 장애물 완성

---
+++ Assets/LeeDaeWoon/Scripts/ObstacleVowel.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ObstacleVowel : MonoBehaviour
+{
+    public enum EObstacle
+    {
+        Obstacle2,
+        Obstacle3,
+        Obstacle4,
+        Obstacle6,
+    }
+    public EObstacle eObstacle;
+    [SerializeField] GameObject obstacle2;
+
+    void Start()
+    {
+        switch (eObstacle)
+        {
+            case EObstacle.Obstacle4:
+                transform.DOLocalMoveX(4, 1).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
+                break;
+        }
+    }
+
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            switch (eObstacle)
+            {
+                case EObstacle.Obstacle2:
+                    obstacle2.transform.DOLocalMoveY(1.5f, 0.5f).SetEase(Ease.Linear).OnComplete(() =>
+                    {
+                        Die();
+                    });
+                    break;
+
+                case EObstacle.Obstacle3:
+                    Die();
+                    break;
+
+                case EObstacle.Obstacle4:
+                    Die();
+                    break;
+
+                case EObstacle.Obstacle6:
+                    Die();
+                    break;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.CompareTag("Player"))
+        {
+            switch (eObstacle)
+            {
+                case EObstacle.Obstacle6:
+                    Die();
+                    break;
+            }
+        }
+    }
+
+    void Die()
+    {
+        Time.timeScale = 0;
+        Debug.Log("Die");
+    }
+}
