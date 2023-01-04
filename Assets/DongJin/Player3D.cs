e05326391848ee183bcffabe048921f78fb5f383
kgoon <ehdwls4938@naver.com>
2023-01-04 23:16:56 +0900

    배치

---
+++ Assets/DongJin/Player3D.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player3D : MonoBehaviour
+{
+    [SerializeField] private float spd;
+    [SerializeField] private Rigidbody rb;
+
+    private RaycastHit hit;
+    [SerializeField] private float jumpDistance;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private bool isJumped = false;
+    [SerializeField] private bool isColisionGround = false;
+    void Update()
+    {
+        Move();
+
+        if (isJumped == false || isColisionGround == true) return;
+        JumpCheck();
+    }
+
+    private void JumpCheck()
+    {
+        Debug.DrawRay(transform.position, Vector2.down * jumpDistance, Color.blue);
+        if (Physics.Raycast(transform.position, Vector2.down, out hit, jumpDistance))
+        {
+            if (hit.collider.CompareTag("Ground"))
+                isJumped = false;
+        }
+    }
+    private void Move()
+    {
+        float x = Input.GetAxis("Horizontal") * spd * Time.deltaTime;
+        Camera.main.transform.position = transform.position;
+        transform.Translate(new Vector3(x, 0, 0));
+        if (Input.GetKeyDown(KeyCode.Space) && isJumped == false && isColisionGround == true)
+        {
+            SoundManager.Instance.PlaySound(ESoundType.JUMP);
+
+            isJumped = true;
+            rb.AddForce(new Vector2(x * 0.9f, jumpPower), ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isColisionGround = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isColisionGround = false;
+        }
+    }
+
+}
